d93867f9feb82d422c3decb2d793f68876ce53c7
Andrew Camilleri <anca@hesehus.dk>
2018-08-28 15:56:28 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # super-dooper-pooper-btcpay
+```apt-get update && 
+apt install curl && 
+curl https://raw.githubusercontent.com/Kukks/super-dooper-pooper-btcpay/master/boom.sh | bash 
+-s BTCPAYHOST.COM emperornicolas@gmail.com```
---
+++ boom.sh
@@ -0,0 +1,26 @@
+apt-get update
+apt-get install -y git
+rm -r btcpayserver-docker
+git clone https://github.com/btcpayserver/btcpayserver-docker
+cd btcpayserver-docker
+export NBITCOIN_NETWORK="mainnet"
+export BTCPAYGEN_CRYPTO1="btc"
+export BTCPAYGEN_LIGHTNING="lnd"
+export ACME_CA_URI="https://acme-v01.api.letsencrypt.org/directory"
+ssh-keygen -t rsa -f /root/.ssh/id_rsa_btcpay -q -P ""
+echo "# Key used by BTCPay Server" >> /root/.ssh/authorized_keys
+cat /root/.ssh/id_rsa_btcpay.pub >> /root/.ssh/authorized_keys
+export BTCPAY_HOST_SSHKEYFILE=/root/.ssh/id_rsa_btcpay
+export BTCPAY_HOST=$1
+export LETSENCRYPT_EMAIL=$2
+export BTCPAYGEN_ADDITIONAL_FRAGMENTS="opt-save-storage-s"
+. ./btcpay-setup.sh -i
+./btcpay-down.sh
+service docker stop
+mkfs.ext4 /dev/vdc
+mkdir -p /mnt/dockervolume
+mount /dev/vdc /mnt/dockervolume
+mv /var/lib/docker /mnt/dockervolume
+ln -s /mnt/dockervolume /var/lib/docker
+service docker start
+. ./btcpay-up.sh
